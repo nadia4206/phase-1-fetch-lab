058a9f91163804b6198c2359f328485cceff4163
Nadia Virjee <nadvirjee@gmail.com>
2022-12-18 16:34:03 -0500

    commit tree eb84bd535d61018e353a054d4fdf6cbfb466350b

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+function fetchBooks(){
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((data) => renderBooks(data))
+  }
 
-function renderBooks(books) {
+
+function renderBooks(fetchBooks) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  fetchBooks.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
